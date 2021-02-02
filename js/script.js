02871122b335e75cddbb46b1c8b4ec0414b60b63
Yuri Gianlorenzi <yurigian@gmail.com>
2021-02-02 10:42:30 +0100

    add user name & avatar from vue

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
+var app = new Vue ({
+  el : '#app',
+  data : {
+    user : {
+      name : 'Yuri Gianlorenzi',
+      avatar : 'img/Saitama.jpg'
+    },//fine dati utente
 
+    contacts : [
+
+    ]//fine dati contatti
+  }//fine data
+});
