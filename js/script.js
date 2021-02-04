e2329d6166694d0e41d8fc1752f1aa2bbaa04ac6
Yuri Gianlorenzi <yurigian@gmail.com>
2021-02-04 12:02:07 +0100

    add function searchContact and relative comments

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ var app = new Vue ({
   data : {
     chatIndex : 0,
     newText : '',
+    search : '',
     user : {
       name : 'Yuri Gianlorenzi',
       avatar : 'img/Saitama.jpg'
@@ -12,7 +13,7 @@ var app = new Vue ({
       {
         name : 'Michele Pel di Carota',
         avatar : 'img/avatar_1.jpg',
-        visible : false,
+        visible : true,
         messages : [
           {
             date : '28/03/2020 10:10:40',
@@ -34,7 +35,7 @@ var app = new Vue ({
       {
         name : 'Marcello TopGun',
         avatar : 'img/avatar_2.jpg',
-        visible : false,
+        visible : true,
         messages : [
           {
             date : '27/03/2020 09:10:40',
@@ -66,7 +67,7 @@ var app = new Vue ({
       {
         name : 'Guglielmo Cancelli',
         avatar : 'img/avatar_3.jpg',
-        visible : false,
+        visible : true,
         messages : [
           {
             date : '29/03/2020 11:30:40',
@@ -93,7 +94,7 @@ var app = new Vue ({
       {
         name : 'Marco Bafforosso',
         avatar : 'img/avatar_4.jpg',
-        visible : false,
+        visible : true,
         messages : [
           {
             date : '26/03/2020 20:10:40',
@@ -120,7 +121,7 @@ var app = new Vue ({
       {
         name : 'Jhonny Jogging',
         avatar : 'img/avatar_5.jpg',
-        visible : false,
+        visible : true,
         messages : [
           {
             date : '30/03/2020 06:10:40',
@@ -132,7 +133,7 @@ var app = new Vue ({
       {
         name : 'Melisandre',
         avatar : 'img/avatar_6.jpg',
-        visible : false,
+        visible : true,
         messages : [
           {
             date : '28/03/2020 10:10:40',
@@ -144,7 +145,7 @@ var app = new Vue ({
       {
         name : 'Uno Qualsiasi',
         avatar : 'img/avatar_7.jpg',
-        visible : false,
+        visible : true,
         messages : [
           {
             date : '28/03/2020 10:10:40',
@@ -179,7 +180,11 @@ var app = new Vue ({
 
       //last info è dunque un array contenete, nello stesso ordine di contacts, l'ultimo messaggio di ogni contatto
       return lastInfo;
-    }
+    },//fine takeLastMessage
+
+    search : function () {
+
+    }//fine search
   },//fine computed
 
   methods : {
@@ -191,19 +196,19 @@ var app = new Vue ({
 
 
       // controllo sull'array per portare tutte le key visible=false
-      this.contacts.forEach((element) => {
-        element.visible = false;
-      });
+      // this.contacts.forEach((element) => {
+      //   element.visible = false;
+      // });
 
       //qui assegnamo all'elemento selezionato, il valore true alla key visible
-      this.contacts[indexContact].visible = true;
+      // this.contacts[indexContact].visible = true;
 
       console.log(this.contacts[indexContact]);
       console.log(this.contacts);
       console.log(this.takeMessages);
     },// fine showChat
 
-    sendMessage (e, chatIndex) {
+    sendMessage (e) {
       // qui creiamo la funzione che inserisce il messaggio digitato nel relativo "contenitore" quando premiamo il tasto invio e abbiamo digitato almeno un carattere
 
       if (this.newText.length > 0 && e.keyCode == 13) {
@@ -220,6 +225,59 @@ var app = new Vue ({
         this.newText = '';
       }
 
-    }//fine sendMessage
+    },//fine sendMessage
+
+    searchContact () {
+      //qui creiamo la funzione che permette la ricerca sui contatti della chat, tramite un v-model "ascoltiamo" quello che l'utente inserisce nell'input (NB SAPENDO CHE OGNI CONTACT HA VISIBLE = true COME CONDIZIONE PER ESSERE STAMPATO NEL V-FOR HTML), con un foreach cicloamo ogni elemento di contacts:
+      //1) se la stringa nella ricerca è presente in contacts.name, la variabile visible rimane true
+      //2) se non è presente, la variabile si modifica in false e quindi non viene stampato (perchè non rispetta la condizione per poter essere stampato)
+
+      this.contacts.forEach(element => {
+          if (element.name.toLowerCase().includes(this.search.toLowerCase())) {
+            element.visible = true;
+            console.log(element);
+          } else {
+            element.visible = false;
+          }
+      });
+    }//fine searchContact
+
   }//fine methods
+
+
 });
+
+
+
+
+
+
+//
+//
+// if (this.search.length > 0 && e.keyCode == 13) {
+//
+//   let spellSearch = this.search.toLowerCase();//per mettere tutto in minuscolo
+//   spellSearch = spellSearch.split('');//per separare ogni elemento di una stringa e inserirlo nello stesso ordine dell'array
+//   console.log(spellSearch);
+//
+//   // let dio = this.contacts.length;
+//
+//   for (let i = 0; i < this.contacts.length; i++) {
+//
+//     let spellName = this.contacts[i].name.toLowerCase();
+//     spellName = spellName.split('');
+//     console.log(spellName);
+//     console.log('i' + i);
+//     for (let j = 0; j < spellSearch.length; j++) {
+//
+//       if (spellSearch[j] === spellName[j]) {
+//         this.searchFinded.push(this.contacts[i]);
+//         this.contacts.splice(i, 1);
+//         console.log(this.searchFinded);
+//         console.log(j);
+//       }
+//     }
+//
+//   }//primo for
+//
+// }//fine if
