b8c3008db9c660f4028368ed1a4523e8910252ae
Yuri Gianlorenzi <yurigian@gmail.com>
2021-02-08 23:52:36 +0100

    add delete single message, BUT if you delete all messages => crash

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ var app = new Vue ({
   data : {
     chatIndex : 0,
     messageIndex : 0,
+    contactIndex : '',
     newText : '',
     search : '',
     writing : '',
@@ -16,21 +17,27 @@ var app = new Vue ({
         name : 'Michele Pel di Carota',
         avatar : 'img/avatar_1.jpg',
         visible : true,
+        openInfoChat : false,
         messages : [
           {
             date : '28/03/2020 10:10:40',
             text: 'La Marianna va in campagna',
-            status: 'received'
+            status: 'received',
+            open : false
           },
           {
             date: '28/03/2020 10:20:10',
             text: 'Sicuro di non aver sbagliato chat?',
-            status: 'sent'
+            status: 'sent',
+            open : false
+
           },
           {
             date: '28/03/2020 16:15:22',
             text: 'Ah scusa!',
-            status: 'received'
+            status: 'received',
+            open : false
+
           }
         ]
       },
@@ -38,31 +45,42 @@ var app = new Vue ({
         name : 'Marcello TopGun',
         avatar : 'img/avatar_2.jpg',
         visible : true,
+        openInfoChat : false,
         messages : [
           {
             date : '27/03/2020 09:10:40',
             text: 'Ma davvero Marianna va in campagna?',
-            status: 'received'
+            status: 'received',
+            open : false
+
           },
           {
             date: '28/03/2020 10:20:10',
             text: 'Sicuro di non aver sbagliato chat?',
-            status: 'sent'
+            status: 'sent',
+            open : false
+
           },
           {
             date: '28/03/2020 16:15:22',
             text: 'Ah scusa!',
-            status: 'received'
+            status: 'received',
+            open : false
+
           },
           {
             date: '28/03/2020 10:20:10',
             text: 'ooooooooooooooooooooooooooooooooooooooooooooooooooooooo',
-            status: 'received'
+            status: 'received',
+            open : false
+
           },
           {
             date: '28/03/2020 16:15:22',
             text: 'Ah scusa!',
-            status: 'received'
+            status: 'received',
+            open : false
+
           }
         ]
       },
@@ -70,26 +88,35 @@ var app = new Vue ({
         name : 'Guglielmo Cancelli',
         avatar : 'img/avatar_3.jpg',
         visible : true,
+        openInfoChat : false,
         messages : [
           {
             date : '29/03/2020 11:30:40',
             text: 'Ma la sai l\'ultima? Marianna va in campagna... labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur',
-            status: 'received'
+            status: 'received',
+            open : false
+
           },
           {
             date: '28/03/2020 16:15:22',
             text: 'Ciao bello Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-            status: 'received'
+            status: 'received',
+            open : false
+
           },
           {
             date: '28/03/2020 10:20:10',
             text: 'ooooooooooooooooooooooooooooooooooooooooooooooooooooooo',
-            status: 'sent'
+            status: 'sent',
+            open : false
+
           },
           {
             date: '28/03/2020 16:15:22',
             text: 'Ah scusa!',
-            status: 'received'
+            status: 'received',
+            open : false
+
           }
         ]
       },
@@ -97,26 +124,35 @@ var app = new Vue ({
         name : 'Marco Bafforosso',
         avatar : 'img/avatar_4.jpg',
         visible : true,
+        openInfoChat : false,
         messages : [
           {
             date : '26/03/2020 20:10:40',
             text: 'Dovremmo trovare il sostituto di Marianna',
-            status: 'received'
+            status: 'received',
+            open : false
+
           },
           {
             date: '28/03/2020 16:15:22',
             text: 'Ciao bello Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-            status: 'sent'
+            status: 'sent',
+            open : false
+
           },
           {
             date: '28/03/2020 10:20:10',
             text: 'ooooooooooooooooooooooooooooooooooooooooooooooooooooooo',
-            status: 'received'
+            status: 'received',
+            open : false
+
           },
           {
             date: '28/03/2020 16:15:22',
             text: 'Ah scusa!',
-            status: 'sent'
+            status: 'sent',
+            open : false
+
           }
         ]
       },
@@ -124,11 +160,14 @@ var app = new Vue ({
         name : 'Jhonny Jogging',
         avatar : 'img/avatar_5.jpg',
         visible : true,
+        openInfoChat : false,
         messages : [
           {
             date : '30/03/2020 06:10:40',
             text: 'Cha bello che è andare a correre con Marianna',
-            status: 'received'
+            status: 'received',
+            open : false
+
           }
         ]
       },
@@ -136,11 +175,14 @@ var app = new Vue ({
         name : 'Melisandre',
         avatar : 'img/avatar_6.jpg',
         visible : true,
+        openInfoChat : false,
         messages : [
           {
             date : '28/03/2020 10:10:40',
             text: 'Ti va di sostituire Marianna',
-            status: 'sent'
+            status: 'sent',
+            open : false
+
           }
         ]
       },
@@ -148,11 +190,14 @@ var app = new Vue ({
         name : 'Uno Qualsiasi',
         avatar : 'img/avatar_7.jpg',
         visible : true,
+        openInfoChat : false,
         messages : [
           {
             date : '28/03/2020 10:10:40',
             text: 'Andrà tutto bene',
-            status: 'received'
+            status: 'received',
+            open : false
+
           }
         ]
       }
@@ -189,19 +234,24 @@ var app = new Vue ({
     },//fine takeTimeMessage
 
     takeLastMessage: function () {
+
       //qui creiamo un array vuoto come variabile, qui verranno pushati, per ogni elemento di contacts, l'ultimo elemento-text dell'array messages, questo sarà il return della function e verrà utilizzato nel ciclo v-for della class chat. essendo un array contenetne l'ultimo messaggio di ogni contact ("convivono"), lo indicizziamo con l'incice del v-for e per ogni contatto, mi restituirà l'ultimo messaggio presente nei data
       let lastInfo = [];
 
-      //usiamo un foreach per ciclare ogni elemento di contacts
-      this.contacts.forEach((element) => {
+      //NECESSARIO INSERIRE DEGLI IF CHE CONDIZIONANO IL COMPUTED PER FARE IN MODO CHE I CALCOLI AL SUO INTERNO VENGANO ESEGFUITI SOLO SE CI SONO MESSAGGI RIFERITI AL CONTATTO DEL CHAT INDEX
+      if (this.contacts[this.chatIndex].messages.length > 0) {
 
-        //creiamo una variabile che per ogni elemento, gli viene assegnato il numero dell'indice dell'ultimo elemento dell'array
-        let positionLast = element.messages.length - 1;
+        //usiamo un foreach per ciclare ogni elemento di contacts
+        this.contacts.forEach((element) => {
 
-        //questo numero viene utilizzato per pushare dentro l'array vuoto il testo dell'utlimo messaggio
-        lastInfo.push(element.messages[positionLast].text);
-      });
+          //creiamo una variabile che per ogni elemento, gli viene assegnato il numero dell'indice dell'ultimo elemento dell'array
+          let positionLast = element.messages.length - 1;
 
+          //questo numero viene utilizzato per pushare dentro l'array vuoto il testo dell'utlimo messaggio
+          lastInfo.push(element.messages[positionLast].text);
+        });
+
+      }
       //last info è dunque un array contenete, nello stesso ordine di contacts, l'ultimo messaggio di ogni contatto
       return lastInfo;
     },//fine takeLastMessage
@@ -209,15 +259,19 @@ var app = new Vue ({
     takeLastTime : function () {
       //qui creiamo un'array che contiene SOLO L'ORARIO, creato usando slice sulla variabile che contiene la stringa data
       let lastTime = [];
-      this.contacts.forEach(element => {
-        let time;
-        let positionLast = element.messages.length - 1;
-        //time = element.messages[positionLast].date.slice(11);//si vedono anche i secondi
-        time = element.messages[positionLast].date.slice(11, 16);// così solo ore e minuti
-        // console.log(time);
-        // time = time.slice(11);
-        lastTime.push(time);
-      });
+
+      //NECESSARIO INSERIRE DEGLI IF CHE CONDIZIONANO IL COMPUTED PER FARE IN MODO CHE I CALCOLI AL SUO INTERNO VENGANO ESEGFUITI SOLO SE CI SONO MESSAGGI RIFERITI AL CONTATTO DEL CHAT INDEX
+      if (this.contacts[this.chatIndex].messages.length > 0) {
+        this.contacts.forEach(element => {
+          let time;
+          let positionLast = element.messages.length - 1;
+          //time = element.messages[positionLast].date.slice(11);//si vedono anche i secondi
+          time = element.messages[positionLast].date.slice(11, 16);// così solo ore e minuti
+          // console.log(time);
+          // time = time.slice(11);
+          lastTime.push(time);
+        });
+      }
       return lastTime;
     }//fine timing
 
@@ -267,7 +321,7 @@ var app = new Vue ({
         // WARNING nella key date, pushamo la data e l'orario usando moment.filter. il filter ci permette di formattare l'orario come quello già presente nei contacs
 
         // con queste indicazioni, utilizzando il chatIndex come riferimento, andiamo nel relativo contatto e pushamo un intero oggetto dentro l'array messages. dentro l'oggetto si trova il testo digitato, lo status 'sent' e la data
-        this.contacts[this.chatIndex].messages.push({date: moment().format("DD/MM/YYYY HH:mm:ss"), text: this.newText, status: 'sent'});
+        this.contacts[this.chatIndex].messages.push({date: moment().format("DD/MM/YYYY HH:mm:ss"), text: this.newText, status: 'sent', open : false});
         // this.contacts[this.chatIndex].messages.
         console.log(this.contacts[this.chatIndex].messages);
 
@@ -279,7 +333,7 @@ var app = new Vue ({
           //qui per eliminare la scritta prima di ricevere il messaggio, in modo da restituire la data dell'ultimo accesso
           this.writing = '';
 
-          this.contacts[this.chatIndex].messages.push({date: moment().format("DD/MM/YYYY HH:mm:ss"), text: 'Error: 404', status: 'received'})
+          this.contacts[this.chatIndex].messages.push({date: moment().format("DD/MM/YYYY HH:mm:ss"), text: 'Error: 404', status: 'received', open : false})
 
         },1500);
 
@@ -306,29 +360,39 @@ var app = new Vue ({
     },//fine searchContact
 
     openMessageInfo (indexMessage) {
-
+      //prendiamo l'indice che ci viene passato dal click e ne assegnamo il valore al messageIndex nei data, ci serve come condizione per far apparire la finestrella, verrà stampata solo quella riferita all'indice passato dal click
       this.messageIndex = indexMessage;
 
-     if (!this.contacts[this.chatIndex].messages[indexMessage].open) {
+      //qui usiamo l'if per creare un "toggle" sul click, in maniera tale che se clicckiamo ed era false, diventa true E VICEVERSA
+      if (this.contacts[this.chatIndex].messages[indexMessage].open) {
+        this.contacts[this.chatIndex].messages[indexMessage].open = false;
+      } else {
+        this.contacts[this.chatIndex].messages[indexMessage].open = true;
+      }
 
-       this.contacts[this.chatIndex].messages[indexMessage].open = true;
-
-     } else {
-       this.contacts[this.chatIndex].messages[indexMessage].open = false;
-
-
-     }
-
+      //qui abbiamo le due condizioni da inserire nel v-if del tag riferito alla finestrella, solo la finestrella che rispetta ENTRAMBE le condizioni, rimarrà aperta. PROBLEMINO può capitare il caso che una finestrella non si apra al primo click
 
 
 
-     console.log(this.messageIndex);
-     // console.log(this.takeMessages);
-     console.log(this.contacts);
+     // console.log(this.messageIndex);
+     // // console.log(this.takeMessages);
+     // console.log(this.contacts);
    },//fine openMessageInfo
 
+   deleteMessage (indexMessage) {
+     this.contacts[this.chatIndex].messages.splice(indexMessage, 1);
+   },//fine deleteMessage
+
    openChatInfo (indexContact) {
-     this.contacts[indexContact].chatInfo = true;
+     this.contactIndex = indexContact;
+
+     if (this.contacts[indexContact].openInfoChat) {
+       this.contacts[indexContact].openInfoChat = false;
+     } else {
+       this.contacts[indexContact].openInfoChat = true;
+     }
+
+     // this.contacts[indexContact].chatInfo = true;
    }
 
   }//fine methods
